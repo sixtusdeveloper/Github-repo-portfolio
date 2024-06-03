3238f6c0854dca5b0e5662fb1e70a705444c3948
Sixtus Aondoakaa <sixtusushrey@gmail.com>
2024-06-03 21:06:54 +0100

    commit on new file

---
+++ src/services.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Our Services</h1>
+        <p>Here are the services we offer:</p>
+        <ul>
+            <li>Service 1</li>
+            <li>Service 2</li>
+            <li>Service 3</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Services',
+    data() {
+        return {
+            // Your data properties here
+        };
+    },
+    methods: {
+        // Your methods here
+    },
+    mounted() {
+        // Code to run when the component is mounted
+    },
+};
+</script>
+
+<style scoped>
+/* Your component-specific styles here */
+</style>
